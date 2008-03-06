3134cfe1c37ba434d82ec9b1fb3acd85704a9eb8
Jack Danger Canty <git@6brand.com>
2008-03-06 09:12:29 -0800

    fixing paths

--- test/test_cached_values.rb
+++ test/test_cached_values.rb
@@ -1,6 +1,5 @@
 require 'test/unit'
-require File.expand_path(File.dirname(__FILE__) + "/test_helper")
-require File.expand_path(File.dirname(__FILE__) + "/leprechaun")
+require File.expand_path(File.dirname(__FILE__) + "/../test_helper")
 
 class CachedValuesTest < Test::Unit::TestCase
   
