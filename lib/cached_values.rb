9983606dee88432361c44b5cfea73ecaf6a62255
Jack Danger Canty <git@6brand.com>
2008-03-12 11:45:48 -0700

    version bump to 1.0.1

--- lib/cached_values.rb
+++ lib/cached_values.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + "/cached_values/cached_value")
 require File.expand_path(File.dirname(__FILE__) + "/cached_values/caches_value")
 
 module CachedValues # :nodoc:
-  VERSION = '1.0.0'
+  VERSION = '1.0.1'
 end
 
 ActiveRecord::Base.send :include, CachesValues
